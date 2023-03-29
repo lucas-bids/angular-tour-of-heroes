caef2308b61dd6e5c541ea611b8e32056c239bd2
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2023-03-29 17:16:09 -0300

    tutorial completed until the 4.Observable data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.sass'],
 })
 export class HeroesComponent {
-    heroes = HEROES;
 
-    selectedHero? : Hero;
-    onSelect(hero:Hero): void {
-      this.selectedHero = hero;
-    }
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
+
