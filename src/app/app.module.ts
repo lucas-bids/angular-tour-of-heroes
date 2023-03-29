ad879b613abf9f71ab085e473bc5e1247bfa7052
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2023-03-29 15:06:53 -0300

    Finished step no. 4 of the tutorial. Heroes list and Individual Hero information are now separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailsComponent,
   ],
   imports: [
     BrowserModule,
