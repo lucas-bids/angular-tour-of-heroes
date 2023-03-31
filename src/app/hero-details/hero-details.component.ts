139a30c6ea2bbefca0ce684b5c261cf95d11dc38
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2023-03-31 16:11:47 -0300

    Section 6. Start on Update heroes

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -18,4 +18,18 @@ export class HeroDetailsComponent {
     private heroService: HeroService,
     private location: Location
   ) {}
+  
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
 }
