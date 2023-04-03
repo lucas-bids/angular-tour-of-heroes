fa416d716a36b3badc94e52d28bc9ae899c304fc
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2023-04-03 16:23:52 -0300

    Finished tutorial. All features added and working.

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -32,4 +32,10 @@ export class HeroDetailsComponent {
   goBack(): void {
     this.location.back()
   }
+
+  save(): void {
+    if(this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
 }
